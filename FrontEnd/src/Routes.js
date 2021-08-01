695658af29f6f4c8a9fd541e26e4c62f79767ebd
MDAmir159 <islamamirul497@gmail.com>
2021-08-01 14:48:37 +0600

    first

---
+++ FrontEnd/src/Routes.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Home from './views/Home'
+import LogIn from './views/LogIn'
+import Settings from './views/Settings'
+import SignUp from './views/SignUp'
+import NewPage from './NewPage'
+import Profile from './views/Home/components/HomeHeader/Profile'
+
+export default function Routes() {
+    return (
+       <Switch>
+           <Route exact path = '/' component = {LogIn} />
+           <Route path = '/signup' component = {SignUp} />
+           <Route path = '/home' component = {Home} />
+           <Route path = '/profile' component = {Profile} />
+           <Route path = '/settings' component = {Settings} />
+       </Switch>
+    )
+}
